dc79f1e395d43662ea4ffb5e52d8469dd8739420
nick stilwell <nickstilwell52@gmail.com>
2023-06-25 10:14:29 +0000

    add tweet char counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+let tweetCharCount = 140;
+
+const addCharCountEventHandler = () => {
+  $("#tweet-textarea").on('input', function() {
+    tweetCharCount = (140 - this.textLength);
+    const counterColor = tweetCharCount < 0 ? "red" : "false";
+    const theCounter = $(this).parent().next().find(".counter");
+    $(theCounter).text(tweetCharCount);
+    $(theCounter).attr("changeColor", counterColor);
+  });
+  console.log('added CharCountEventHandler');
+}
+
+$(document).ready(() => {
+  addCharCountEventHandler();
+});
